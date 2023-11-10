9faba8ce77752471e51f674ef272307af76f3fce
prabhjyot009 <prabh156dps@gmail.com>
2023-11-11 02:21:54 +0530

    Add animations and image to HeroContent component

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,8 +1,9 @@
 "use client"
 import React from "react"
 import {motion} from "framer-motion"
-import { slideInFromTop } from "@/utils/motion"
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion"
 import { SparklesIcon } from "@heroicons/react/20/solid"
+import Image from "next/image"
 
 const HeroContent = () => {
   return (
@@ -14,14 +15,46 @@ const HeroContent = () => {
         <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
             <motion.div
             variants={slideInFromTop}
-            className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+            className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
             >
                 <SparklesIcon className="h-5 w-5 text-[#b49bff] mr-[10px]" />
                 <h1 className="Welcome-text text-[13px]">Frontend Developer Portfolio</h1>
             </motion.div>
+                <motion.div 
+                variants={{...slideInFromLeft(0.5)}}
+                className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+                >
+                  Providing
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">the best</span>
+                  project experience
+                </motion.div>
+                <motion.p 
+                variants={{...slideInFromLeft(0.8)}}
+                className="text-lg text-gray-400 my-5 max-w-[600px]"
+                >
+                  I&apos;m a Frontend Developer with experience in Website,
+                  Mobile Application, and Software development. Check out my projects and skills.
+                </motion.p>
+                <motion.a
+                variants={{...slideInFromLeft(1)}}
+                className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px"
+                >
+                  Learn More!
+                </motion.a>
         </div>
+        <motion.div
+        variants={{...slideInFromRight(0.8)}}
+        className="w-full h-full flex justify-center items-center"
+        >
+          <Image
+          src="/mainIconsDark.svg"
+          alt="work Icons"
+          width={650}
+          height={650}
+          />
+        </motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
 export default HeroContent
